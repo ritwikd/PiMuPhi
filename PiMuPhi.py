99907ba94c448c656f9a7d0598f2d7619b5deeb0
Ritwik Dutta <ritzymail@gmail.com>
2018-04-24 19:22:08 -0400

    Updated code, new color system is HELLA LIT.

--- PiMuPhi.py
+++ PiMuPhi.py
@@ -2,23 +2,47 @@ import numpy as np
 import pyaudio
 import math
 import pigpio
-from time import sleep
+from time import sleep, time
 
 class PiMuPhi:
     
+    color_change_enabled = True
+    intensity_change_enabled = True
+    
+    current_index = 0
+    change_threshold = 50
+    
     sound_format = pyaudio.paFloat32
-    chunk = 2048
+    chunk = 256
     start = 0
-    slices = 512
+    slices = 100
     
-    min_b = 0.4
-    max_b = 14
+    min_b = 1.5
+    max_b = 13
     
-    bass_div = 150
-    mid_div  = 80
-    treb_div = 240
     
-    loop_delay = 0.02
+    bb_vals = [0] * 128
+    avg_beat_bass = 0
+    
+    rti_vals = [0] * 28
+    avg_rti = 0
+    
+    rmi_vals = [0] * 28
+    avg_rmi = 0
+    
+    i_vals = [0] * 10
+    avg_intensity = 0
+    
+    m_vals = [0] * 10
+    avg_lvl_mid = 0
+    
+    t_vals = [0] * 10
+    avg_lvl_treble = 0
+    
+    dc_vals = [0] * 25
+    avg_intensity_slow = 0
+    
+    loop_delay = 0.0
     
     test_on_startup = False
 
@@ -45,17 +69,80 @@ class PiMuPhi:
 
     def fast_fourier_transform(self):
         spec_raw = np.fft.fft(self.spectrum[self.start:self.start + self.slices]) 
-        self.spec_proc = list(map(lambda f: np.sqrt(f.real**2 + f.imag**2), spec_raw))
+        self.spec_proc = list(map(lambda f: np.sqrt(f.real ** 2 + f.imag**2), spec_raw))
 
-    def get_color(self, b, m, t):
-        #base_col = [0.10, 0.15, 0.95]
-        base_col = [1.0, 0.0, 0.0]
-        return base_col
+    def get_color(self, t, i):
+        
+        base_colors = [
+            [0.05, 0.05, 0.95],
+            [0.70, 0.05, 0.85],
+            [0.89, 0.05, 0.05]
+        ]
+        
+        base_col = [0, 0, 1]
+        
+        base_col[0] += (self.avg_rti) * 5.0
+        
+        base_col[1] += (self.avg_rmi) * 3.0
+        
+        base_col[2] -= ((self.avg_rmi + self.avg_rti)) * 4.0
+        
+        return self.normalize_color(base_col)
+        
+        #return self.normalize_color(base_colors[self.current_index])
+    
+    def normalize_color(self, col):
+        col[0] = 0 if col[0] < 0 else col[0]
+        col[0] = 1.0 if col[0] > 1 else col[0]
+        
+        col[1] = 0 if col[1] < 0 else col[1]
+        col[1] = 1.0 if col[1] > 1 else col[1]
+        
+        col[2] = 0 if col[2] < 0 else col[2]
+        col[2] = 1.0 if col[2] > 1 else col[2]
+        
+        return col
+    
+    def get_running_avg(self, bb, intensity, t, m):
+        for i in range(len(self.i_vals) - 1):
+            self.i_vals[i] = self.i_vals[i + 1]
+        self.i_vals[-1] = intensity
+        self.avg_intensity_fast = sum(self.i_vals)/len(self.i_vals)
+        
+        for i in range(len(self.dc_vals) - 1):
+            self.dc_vals[i] = self.dc_vals[i + 1]
+        self.dc_vals[-1] = intensity
+        self.avg_intensity_slow = sum(self.dc_vals)/len(self.dc_vals)
+        
+        for i in range(len(self.bb_vals) - 1):
+            self.bb_vals[i] = self.bb_vals[i + 1]
+        self.bb_vals[-1] = bb
+        self.avg_beat_bass = sum(self.bb_vals)/len(self.bb_vals)
+        
+        for i in range(len(self.t_vals) - 1):
+            self.t_vals[i] = self.t_vals[i + 1]
+        self.t_vals[-1] = t
+        self.avg_lvl_treble = sum(self.t_vals)/len(self.t_vals)
+        
+        for i in range(len(self.m_vals) - 1):
+            self.m_vals[i] = self.m_vals[i + 1]
+        self.m_vals[-1] = m
+        self.avg_lvl_mid = sum(self.m_vals)/len(self.m_vals)
+        
+        for i in range(len(self.rti_vals) - 1):
+            self.rti_vals[i] = self.rti_vals[i + 1]
+        self.rti_vals[-1] = t/i
+        self.avg_rti = sum(self.rti_vals)/len(self.rti_vals)
+        
+        for i in range(len(self.rmi_vals) - 1):
+            self.rmi_vals[i] = self.rmi_vals[i + 1]
+        self.rmi_vals[-1] = m/i
+        self.avg_rmi = sum(self.rmi_vals)/len(self.rmi_vals)
         
     def normalize_brightness(self, n):
-		n = 0 if n < 0 else n
-		n = 255 if n > 255 else n
-		return n
+        n = 0 if n < 0 else n
+        n = 255 if n > 255 else n
+        return n
     
     def get_brightness(self, n):
         n = self.min_b if n < self.min_b else n
@@ -64,9 +151,9 @@ class PiMuPhi:
         return self.normalize_brightness(brightness)
         
     def setcol(self, r, g, b):
-		self.pi.set_PWM_dutycycle(23, 255 - r)
-		self.pi.set_PWM_dutycycle(24, 255 - g)
-		self.pi.set_PWM_dutycycle(25, 255 - b)
+        self.pi.set_PWM_dutycycle(23, 255 - r)
+        self.pi.set_PWM_dutycycle(24, 255 - g)
+        self.pi.set_PWM_dutycycle(25, 255 - b)
 
     def run(self):
         try:
@@ -96,27 +183,48 @@ class PiMuPhi:
                 self.spectrum = self.read_audio()
                 self.fast_fourier_transform()
                 
-                bass_val = sum(self.spec_proc[ 10:172])/self.bass_div
-                mid_val  = sum(self.spec_proc[172:400])/self.mid_div
-                treb_val = sum(self.spec_proc[400:512])/self.treb_div
+                beat_bass = self.spec_proc[0] + self.spec_proc[1]
                 
-                color = self.get_color(bass_val, mid_val, treb_val)
-                brightness = self.get_brightness(bass_val)
+                intensity = round(sum(self.spec_proc[:25])/20) + 0.001
+                midrange = round(sum(self.spec_proc[25:70])/45) + 0.001
+                treble = round(sum(self.spec_proc[70:])/30) + 0.001
+                
+                self.get_running_avg(beat_bass, intensity, treble, midrange)
+                self.b_max = self.avg_intensity_slow * 1.5
+                
+                if self.avg_beat_bass < self.change_threshold/2:
+                    self.change_threshold -= 5
+                
+                if beat_bass > self.change_threshold:
+                    self.current_index += 1
+                    self.current_index = 0 if self.current_index > 2 else self.current_index
+                
+                self.change_threshold = self.avg_beat_bass * 2.0
+                
+                color = self.get_color(treble, intensity)
+                color = color if self.color_change_enabled else [1,1,1] 
+                
+                brightness = self.get_brightness(self.avg_intensity_fast)
+                brightness = brightness if self.intensity_change_enabled else 255
                 
                 r_proc = brightness * color[0]
                 g_proc = brightness * color[1]
                 b_proc = brightness * color[2]
+                #if (beat_bass > self.change_threshold):
+                 #   print(beat_bass, self.change_threshold)
                 
                 self.setcol(r_proc, g_proc, b_proc)
-
-                print bass_val, mid_val, treb_val
                 
                 sleep(self.loop_delay)
                 
+                
 
         except KeyboardInterrupt:
-            self.pa.close() 
+            print("Exiting PiMuPhi.")
+            self.setcol(0,0,0)
+            self.stream.close()
+            self.pa.terminate() 
 
 if __name__ == "__main__":
     light_box = PiMuPhi()
-    
+
